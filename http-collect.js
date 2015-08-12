18b33dc631749775f99aea3b39236c0a744adb92
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-08-12 16:19:31 -0430

    Añadiendo la solución del ejercicio de http-collect

---
+++ http-collect.js
@@ -0,0 +1,15 @@
+var bl = require('bl');
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function(response){
+  response.pipe(bl(function(err, data){
+      if (err)
+        return console.error(err);
+      var data = data.toString();
+      console.log(data.length);
+      console.log(data);
+    })
+  );
+})
